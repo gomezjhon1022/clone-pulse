8a75a642831f891cca997c09f49c6daecbef7a5e
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-02-05 22:42:03 -0500

    Add: Hamburger menu

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 import './Header.css'
 
+
 function Header () {
+  const [isMenuMobileOpen, setIsMenuMobileOpen] = useState(false)
+  
+  function handleMenuMobile () {
+    setIsMenuMobileOpen(!isMenuMobileOpen);
+    console.log("cambio")
+  }
   return (
     <header className="header">
       <div className="container">
@@ -8,6 +16,11 @@ function Header () {
           <a href='/'>
             <img src={`https://pulseapp.com/img/pulse-logo.svg`} />
           </a>
+          <div className='header__menu__mobile'>
+            <button onClick={handleMenuMobile}>
+            <svg className="menu-image h-8 w-8" viewBox="0 0 35 22" xmlns="http://www.w3.org/2000/svg"><title>Menu - Open</title><path d="M0 0h35v2H0zM0 10h35v2H0zM0 20h35v2H0z"></path></svg>
+            </button>
+          </div>
           <ul className='header__right'>
             <li>
               <a>
