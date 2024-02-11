1815eef364fc940fd5b41006406ec9bf0a89e798
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-02-10 21:13:31 -0500

    Add: Section pricing books

---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,41 @@
+import './Home.css'
+
+export function Home () {
+  return (
+    <main>
+      <section className="content">
+        <div className="container">
+          <div className='content-try'>
+            <h1>Try Pulse free for <br/>30 days</h1>
+            <p>See why Pulse is the best way to monitor your cash flow. Sign up for any plan and try Pulse absolutely free for 30 days.</p>
+          </div>
+        </div>
+        <div className="container">
+          <div className='pricing-books'>
+            <div className='pricing-books-list'>
+              <div className='container'>
+                <h2>These are what we call the Pulse Basics. They’re what every customer gets.</h2>
+                <ul>
+                  <li>Manage cash flow on a daily, weekly, monthly, or yearly basis</li>
+                  <li>Forecast growth with recurring income or expenses that scale automatically</li>
+                  <li>Works with any currency</li>
+                  <li>See Money In and Money Out, categorize transactions, and run helpful reports</li>
+                  <li>Toggle entries and accounts on and off to game out different scenarios</li>
+                </ul>
+                <p>Just need the Pulse Basics? <br/>
+                  <a>Get them now</a> for $29 per month.
+                </p>
+              </div>
+            </div>
+            <divp className='pricing-books-plan'></divp>
+            <divp className='pricing-books-features'></divp>
+          </div>
+        </div>
+        <div className="container">
+          try pulse3
+        </div>
+      </section>
+
+    </main>
+  )
+}
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,11 +1,12 @@
 import { Layout } from '../../Components/Layout'
+import { Home } from '../../Components/Home'
 
 function App() {
 
   return (
     <div className='App'>
       <Layout>
-      
+        <Home />
       </Layout>
     </div>
   )
