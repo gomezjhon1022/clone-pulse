76694e5ee8c05bc5f3248f15fdd358401e8aa5fd
Jhon Gomez <jhonjairogomez92@gmail.com>
2024-02-11 22:20:15 -0500

    Add: Section recommended

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -4,7 +4,7 @@ export function Home () {
   return (
     <main>
       <section className="content">
-        <div className="container">
+        <div className="container container-try">
           <div className='content-try'>
             <h1>Try Pulse free for <br/>30 days</h1>
             <p>See why Pulse is the best way to monitor your cash flow. Sign up for any plan and try Pulse absolutely free for 30 days.</p>
@@ -15,7 +15,7 @@ export function Home () {
             <div className='pricing-books-list'>
               <div className='container'>
                 <h2>These are what we call the Pulse Basics. They’re what every customer gets.</h2>
-                <ul>
+                <ul className='benefits'>
                   <li>Manage cash flow on a daily, weekly, monthly, or yearly basis</li>
                   <li>Forecast growth with recurring income or expenses that scale automatically</li>
                   <li>Works with any currency</li>
@@ -25,6 +25,46 @@ export function Home () {
                 <p>Just need the Pulse Basics? <br/>
                   <a>Get them now</a> for $29 per month.
                 </p>
+              </div>
+              <div className='recommended'>
+                <a href='/'>
+                  <div>
+                    <div className='block-banner'>
+                      RECOMMENDED
+                    </div>
+                    <div className='recommended-main'>
+                      <div>
+                        <div className='title'>Small Business Plan</div>
+                        <div className='price'> $59 per month </div>
+                        <div style={{display:'none'}}>
+                          <div >
+                            Sign Up Now
+                          </div>
+                        </div>
+                        <div className='extra'> You get Pulse Basics, plus: </div>
+                      </div>
+                    </div>
+                    <ul className='recommended-list'>
+                      <li>
+                        <span>Manage cash flow across multiple financial accounts</span>
+                      </li>
+                      <li>
+                        <span>Invite your investors, book keeper, or management team to see reports or manage cash flow</span>
+                      </li>
+                      <li>
+                        <span>Integrate with QuickBooks Online for more accurate cash flow</span>
+                      </li>
+                      <li>
+                        <span>Track your actual cash flow alongside your projected cash flow</span>
+                      </li>
+                    </ul>
+                    <div className='recommended-sign-up'>
+                      <div>
+                      Sign Up Now
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
             </div>
             <divp className='pricing-books-plan'></divp>
